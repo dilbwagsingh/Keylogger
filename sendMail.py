cddb7fdcef79ccf6da268fa84dc31a3ab18b0ff3
dilbwagsingh <dilbwagsinghb.met18@itbhu.ac.in>
2019-06-30 23:23:04 +0530

    :octocat:Mailing system added

---
+++ sendMail.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+email_user = 'dilbwagsinghb.met18@itbhu.ac.in'
+email_password = 'hello757'
+email_send = 'dilbwagsingh@gmail.com'
+
+subject = 'Trail'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'Hi there, sending this email from Python!'
+msg.attach(MIMEText(body,'plain'))
+
+filename='log.txt'
+attachment  =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(email_user,email_password)
+
+
+server.sendmail(email_user,email_send,text)
+server.quit()
